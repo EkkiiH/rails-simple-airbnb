e23f5640c6068b9f227b58c637f31d3629939e50
Zyania Alvarez <zyania.alvarez@gmail.com>
2023-08-23 16:52:58 +0200

    starting app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts 'Creating 4 fake flats...'
+4.times do
+  flat = Flat.new(
+    name: Faker::Lorem.sentence,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: Faker::Number.between(from: 35, to: 250),
+    number_of_guests: Faker::Number.between(from: 1, to: 12)
+  )
+  flat.save!
+end
+puts 'Finished!'
